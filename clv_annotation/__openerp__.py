f5778d9da534beae6a76f73e9ed3e7c66703bdde
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2014-09-02 13:12:54 -0300

    Inclusion of module clv_annotation.

---
+++ clv_annotation/__openerp__.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+################################################################################
+#                                                                              #
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol                  #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU Affero General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU Affero General Public License for more details.                          #
+#                                                                              #
+# You should have received a copy of the GNU Affero General Public License     #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+################################################################################
+
+{
+    'name': 'Annotation',
+    'version': '1.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'http://clvsol.com',
+    'description': '''
+Annotation
+==========
+    ''',
+    'depends': [
+        'clv_base',
+        'clv_tag',
+        ],
+    'data': [
+        'security/clv_annotation_security.xml',
+        'security/ir.model.access.csv',
+        'clv_annotation_view.xml',
+        'category/clv_annotation_category_view.xml',
+        'clv_tag/clv_tag_view.xml',
+        'seq/clv_annotation_sequence.xml',
+        'seq/clv_annotation_category_sequence.xml',
+        'wkf/clv_annotation_workflow.xml',
+        'wkf/clv_annotation_wkf_view.xml',
+        'history/clv_annotation_history_view.xml',
+        ],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'active': False,
+}
---
+++ clv_annotation/clv_annotation.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+################################################################################
+#                                                                              #
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol                  #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU Affero General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU Affero General Public License for more details.                          #
+#                                                                              #
+# You should have received a copy of the GNU Affero General Public License     #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+################################################################################
+
+from openerp import models, fields, api
+from openerp.osv import osv
+from datetime import *
+
+class clv_annotation(models.Model):
+    _name = 'clv_annotation'
+
+    name = fields.Char('Subject', size=64, select=1, required=True)
+    code = fields.Char ('Annotation Code',size=128, required=False)
+    author = fields.Many2one('res.users', 'Author', required=True, readonly=True,
+                             default=lambda self: self._uid)
+    date = fields.Datetime("Date", required=True, readonly=True,
+                           default=lambda *a: datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    body = fields.Text(string='Body')
+    active = fields.Boolean('Active', 
+                            help="If unchecked, it will allow you to hide the annotation without removing it.",
+                            default=1)
+    
+    _sql_constraints = [
+        ('uniq_annotation_code', 'unique(code)', "Error! The annotation code must be unique!"),
+        ]
+
+    _order = "date desc"
+
