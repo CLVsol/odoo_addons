769f16fdb6259d5da2d4eaa75c04cfa40ade2df8
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2014-10-14 21:23:09 -0300

    Updated module clv_tag.

--- clv_tag/__init__.py
+++ clv_tag/__init__.py
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ################################################################################
 
-import clv_tag
-import seq
-import wkf
-import history
+# import clv_tag
+# import seq
+# import wkf
+# import history
