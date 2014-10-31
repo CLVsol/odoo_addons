f586b54c56f5603ac748a60822adf67ce8cd2e41
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2014-10-31 18:23:25 -0200

    Included module clv_medicament (from clvsol_odoo_addons branch 7.0-oehealth - oehealth_medicament).

---
+++ clv_medicament/__openerp__.py
@@ -0,0 +1,74 @@
+# -*- encoding: utf-8 -*-
+################################################################################
+#                                                                              #
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol                  #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU Affero General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU Affero General Public License for more details.                          #
+#                                                                              #
+# You should have received a copy of the GNU Affero General Public License     #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+################################################################################
+
+{
+    'name': 'Medicament',
+    'version': '1.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'http://clvsol.com',
+    'description': '''
+Medicament
+==========
+    ''',
+    'images': [],
+    'depends': [
+        'clv_base',
+        'clv_tag',
+        'clv_annotation',
+        #'product',
+        ],
+    'data': [
+        # 'security/clv_medicament_security.xml',
+        # 'security/ir.model.access.csv',
+        # 'clv_medicament_view.xml',
+        # 'category/clv_medicament_category_view.xml',
+        # 'clv_tag/clv_tag_view.xml',
+        # 'clv_annotation/clv_annotation_view.xml',
+        # 'seq/clv_medicament_sequence.xml',
+        # 'seq/clv_medicament_category_sequence.xml',
+        # 'wkf/clv_medicament_workflow.xml',
+        # 'wkf/clv_medicament_wkf_view.xml',
+        # 'history/clv_medicament_history_view.xml',
+        # 'product_product_view.xml',
+        # 'oehealth_annotation_view.xml',
+        # 'security/oehealth_medicament_security.xml',
+        # 'oehealth_medicament_workflow.xml',
+        # 'oehealth_medicament_view.xml',
+        # 'oehealth_medicament_sequence.xml',
+        # 'oehealth_medicament_category_view.xml',
+        # 'oehealth_medicament_manufacturer_view.xml',
+        # 'oehealth_medicament_therapeutic_class_view.xml',
+        # 'oehealth_medicament_active_component_view.xml',
+        # 'oehealth_tag_view.xml',
+        # 'oehealth_drug_form_view.xml',
+        # 'oehealth_drug_route_view.xml',
+        # 'oehealth_medicament_template_view.xml',
+        # 'oehealth_medicament_template_sequence.xml',
+        ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'active': False,
+    'css': [],
+}
