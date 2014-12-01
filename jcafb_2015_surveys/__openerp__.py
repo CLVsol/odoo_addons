36373257b1c8fd67107f9c74ec68cce70ccb580e
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2014-12-01 16:59:12 -0200

    Included module "jcafb_2015_surveys".

---
+++ jcafb_2015_surveys/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+################################################################################
+#                                                                              #
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol                  #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU Affero General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU Affero General Public License for more details.                          #
+#                                                                              #
+# You should have received a copy of the GNU Affero General Public License     #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+################################################################################
+
+{
+    'name': 'JCAF 2015 Surveys',
+    'version': '1.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'http://clvsol.com',
+    'description': '''
+This module will install all the JCAF 2015 surveys.
+    ''',
+    'depends': [
+        'survey',
+        ],
+    'data': [
+        'survey_jcafb_2015_data.xml',
+        #'survey_jcafb_QSE15.xml',
+        'survey_jcafb_FSE15.xml',
+        'survey_jcafb_ISE15.xml',
+        'survey_jcafb_CSE15.xml',
+        'survey_jcafb_QMD15.xml',
+        'survey_jcafb_QAN15.xml',
+        'survey_jcafb_QDH15.xml',
+        ],
+    'test': [],
+    'installable': True,
+    'active': False,
+}
