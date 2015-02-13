57e67b704d71a93a258e9c1cb5a17083a09b1e4c
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-02-13 12:14:34 -0200

    Updated "clv_batch".

--- clv_batch/placement/__init__.py
+++ clv_batch/placement/__init__.py
@@ -17,10 +17,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ################################################################################
 
-import clv_batch_placement
-import clv_batch_placement_place
-import clv_batch_placement_frame
-import clv_batch_placement_tray
+# import clv_batch_placement
+# import clv_batch_placement_place
+# import clv_batch_placement_frame
+# import clv_batch_placement_tray
 import clv_batch_place
 import clv_batch_frame
 import clv_batch_tray
