a985e9b5e342c41f4408c0c4e1caf710312bbb19
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-02-23 13:47:41 -0300

    Included module "clv_insured_card".

---
+++ clv_insured_card/__openerp__.py
@@ -0,0 +1,61 @@
+# -*- encoding: utf-8 -*-
+################################################################################
+#                                                                              #
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol                  #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU Affero General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU Affero General Public License for more details.                          #
+#                                                                              #
+# You should have received a copy of the GNU Affero General Public License     #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+################################################################################
+
+{
+    'name': 'Insured Card',
+    'version': '1.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'http://clvsol.com',
+    'description': '''
+Insured Card
+============
+    ''',
+    'images': [],
+    'depends': [
+        'clv_base',
+        'clv_tag',
+        'clv_annotation',
+        'clv_insured',
+        ],
+    'data': [
+        'security/clv_insured_card_security.xml',
+        'security/ir.model.access.csv',
+        'clv_insured_card_view.xml',
+        'category/clv_insured_card_category_view.xml',
+        'clv_tag/clv_tag_view.xml',
+        'clv_annotation/clv_annotation_view.xml',
+        'seq/clv_insured_card_sequence.xml',
+        'seq/clv_insured_card_category_sequence.xml',
+        'wkf/clv_insured_card_workflow.xml',
+        'wkf/clv_insured_card_wkf_view.xml',
+        'history/clv_insured_card_history_view.xml',
+        'menu/clv_insured_card_menu_view.xml',
+        'clv_insured/clv_insured_view.xml',
+        ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'active': False,
+    'css': [],
+}
---
+++ clv_insured_card/clv_insured_card.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+################################################################################
+#                                                                              #
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol                  #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU Affero General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU Affero General Public License for more details.                          #
+#                                                                              #
+# You should have received a copy of the GNU Affero General Public License     #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+################################################################################
+
+from openerp import models, fields, api
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+class clv_insured_card(models.Model):
+    _name = 'clv_insured_card'
+
+    name = fields.Char('Printed Name', required=True, size=35, help='Name printed on the card.')
+    code = fields.Char(size=64, string='Insured Code')
+    notes = fields.Text(string='Notes')
+    date_inclusion = fields.Datetime("Inclusion Date", required=False, readonly=False,
+                                     default=lambda *a: datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    active = fields.Boolean('Active', 
+                            help="If unchecked, it will allow you to hide the insured card without removing it.",
+                            default=1)
+
+    _order='name'
+
+    _sql_constraints = [('code_uniq', 'unique(code)', u'Error! The Insured Card Code must be unique!')]
