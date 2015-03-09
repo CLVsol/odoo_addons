39e373d61ab8affdca84ade4a59aa0082ccf57ac
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-03-09 15:08:41 -0300

    Updated "clv_insured_mng".

--- clv_insured_mng/clv_insured_mng.py
+++ clv_insured_mng/clv_insured_mng.py
@@ -26,6 +26,7 @@ class clv_insured_mng(models.Model):
 
     insured_id = fields.Many2one('clv_insured', 'Insured', ondelete='restrict')
     name = fields.Char('Name', required=True, size=64)
+    display_name = fields.Char('Name', compute='_compute_display_name',)
     alias = fields.Char('Alias', size=64, help='Common name that the Insured is referred')
     code = fields.Char(size=64, string='Insured Code')
     # address_id = fields.Many2one('res.partner', 'Working Address', ondelete='restrict')
