a918e808903cdb8c2c700c42519a534087117ad0
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-03-19 13:10:41 -0300

    Included module "clv_seedling_batch".

---
+++ clv_seedling_batch/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- encoding: utf-8 -*-
+################################################################################
+#                                                                              #
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol                  #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU Affero General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU Affero General Public License for more details.                          #
+#                                                                              #
+# You should have received a copy of the GNU Affero General Public License     #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+################################################################################
+
+{
+    'name': 'Seedlilng Batch',
+    'version': '1.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'http://clvsol.com',
+    'description': '''
+Seedlilng Batch
+===============
+    ''',
+    'images': [],
+    'depends': [
+        'clv_batch',
+        ],
+    'data': [
+        'clv_batch_view.xml',
+        'menu/clv_batch_menu_view.xml',
+        ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'active': False,
+    'css': [],
+}
