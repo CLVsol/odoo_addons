1a1c86550497d50586011241848feb165ed14c07
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-04-15 09:11:01 -0300

    Included module "clv_medicament_prescription".

---
+++ clv_medicament_prescription/category/clv_medicament_prescription_category.py
@@ -0,0 +1,109 @@
+# -*- encoding: utf-8 -*-
+################################################################################
+#                                                                              #
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol                  #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU Affero General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU Affero General Public License for more details.                          #
+#                                                                              #
+# You should have received a copy of the GNU Affero General Public License     #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+################################################################################
+
+from openerp.osv import fields, osv
+
+class clv_medicament_prescription_category(osv.osv):
+    _name = 'clv_medicament_prescription.category'
+
+    def name_get(self, cr, uid, ids, context=None):
+        """Return the category's display name, including their direct
+           parent by default.
+
+        :param dict context: the ``category_display`` key can be
+                             used to select the short version of the
+                             tag (without the direct parent),
+                             when set to ``'short'``. The default is
+                             the long version."""
+        if context is None:
+            context = {}
+        if context.get('category_display') == 'short':
+            return super(tag, self).name_get(cr, uid, ids, context=context)
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        reads = self.read(cr, uid, ids, ['name', 'parent_id'], context=context)
+        res = []
+        for record in reads:
+            name = record['name']
+            if record['parent_id']:
+                name = record['parent_id'][1] + ' / ' + name
+            res.append((record['id'], name))
+        return res
+
+    def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=100):
+        if not args:
+            args = []
+        if not context:
+            context = {}
+        if name:
+            name = name.split(' / ')[-1]
+            ids = self.search(cr, uid, [('name', operator, name)] + args, limit=limit, context=context)
+        else:
+            ids = self.search(cr, uid, args, limit=limit, context=context)
+        return self.name_get(cr, uid, ids, context)
+
+    def _name_get_fnc(self, cr, uid, ids, prop, unknow_none, context=None):
+        res = self.name_get(cr, uid, ids, context=context)
+        return dict(res)
+
+    _columns = {
+        'name': fields.char('Category', required=True, size=64, translate=True),
+        'parent_id': fields.many2one('clv_medicament_prescription.category', 'Parent Category', select=True, ondelete='restrict'),
+        'code': fields.char ('Category Code',size=128, required=False),
+        'description': fields.char(string='Description', size=256),
+        'notes': fields.text(string='Notes'),
+        'complete_name': fields.function(_name_get_fnc, type="char", string='Full Category', store=False),
+        'child_ids': fields.one2many('clv_medicament_prescription.category', 'parent_id', 'Child Categories'),
+        'active': fields.boolean('Active', 
+                                 help="If unchecked, it will allow you to hide the category without removing it."),
+        'parent_left': fields.integer('Left parent', select=True),
+        'parent_right': fields.integer('Right parent', select=True),
+        'medicament_prescription_ids': fields.many2many('clv_medicament', 
+                                                        'clv_medicament_prescription_category_rel', 
+                                                        'category_id', 
+                                                        'medicament_id', 
+                                                        'medicaments'),
+        }
+
+    _sql_constraints = [
+        ('uniq_category_code', 'unique(code)', "Error! The Category Code must be unique!"),
+        ]
+
+    _constraints = [
+        (osv.osv._check_recursion, 'Error! You can not create recursive categories.', ['parent_id'])
+        ]
+    
+    _defaults = {
+        'active': 1,
+        }
+    
+    _parent_store = True
+    _parent_order = 'name'
+    _order = 'parent_left'
+
+class clv_medicament_prescription(osv.osv):
+    _inherit = 'clv_medicament_prescription'
+
+    _columns = {
+        'category_ids': fields.many2many('clv_medicament_prescription.category', 
+                                         'clv_medicament_prescription_category_rel', 
+                                         'medicament_id', 
+                                         'category_id', 
+                                         'Categories'),
+        }
---
+++ clv_medicament_prescription/clv_medicament_prescription.py
@@ -0,0 +1,61 @@
+# -*- encoding: utf-8 -*-
+################################################################################
+#                                                                              #
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol                  #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify         #
+# it under the terms of the GNU Affero General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# This program is distributed in the hope that it will be useful,              #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU Affero General Public License for more details.                          #
+#                                                                              #
+# You should have received a copy of the GNU Affero General Public License     #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
+################################################################################
+
+from openerp.osv import fields, osv
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+class clv_medicament_prescription(osv.Model):
+    _name='clv_medicament_prescription'
+
+    def _age(self, cr, uid, ids, field_name, arg, context={}):
+        result = {}
+        now = datetime.now()
+        for r in self.browse(cr, uid, ids, context=context):
+            if r.prescription_date:
+                dob = datetime.strptime(r.prescription_date,'%Y-%m-%d')
+                delta=relativedelta (now, dob)
+                result[r.id] = str(delta.years) +"y "+ str(delta.months) +"m "+ str(delta.days)+"d"
+            else:
+                result[r.id] = "No Date of Birth!"
+        return result
+
+    _columns={
+        'name': fields.char(size=256, string='Prescription Code', required=True,
+                            help='Type in the Code of this prescription'),
+        'prescription_date': fields.date(string='Prescription Date', required=False),
+        'age': fields.function(_age, type="char", string='Age', store=False),
+        'notes': fields.text(string='Prescription Notes'),
+        'active': fields.boolean('Active', help="The active field allows you to hide the prescription without removing it."),
+        'transcription_date': fields.date(string='Transcription Date', required=False),
+        'prescriber_id': fields.many2one('clv_professional', string='Prescriber', required=False),
+        'insured_card_id': fields.many2one('clv_insured_card', string='Insured Card', required=False),
+        'transcriber' : fields.many2one ('res.users', 'Transcriber'),
+        }
+    
+    _sql_constraints = [
+        ('uniq_name', 'unique(name)', "Error! The Prescription Code must be unique!"),
+        ]
+
+    _defaults={
+        'prescription_date': lambda *a: datetime.now().strftime('%Y-%m-%d'),
+        'active': 1,
+        'transcription_date': lambda *a: datetime.now().strftime('%Y-%m-%d'),
+        'transcriber': lambda obj,cr,uid,context: uid, 
+        }
