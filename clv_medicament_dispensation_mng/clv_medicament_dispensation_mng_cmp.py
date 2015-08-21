7d395bce5cfa931ecf4cbc1eb3e0633abc9701b6
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-08-21 13:12:40 -0300

    Updated "clv_medicament_dispensation_mng".

--- clv_medicament_dispensation_mng/clv_medicament_dispensation_mng_cmp.py
+++ clv_medicament_dispensation_mng/clv_medicament_dispensation_mng_cmp.py
@@ -17,15 +17,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ################################################################################
 
-import clv_medicament_dispensation_mng
-import clv_medicament_dispensation_mng_cmp
-# import category
-# import clv_tag
-# import clv_annotation
-import seq
-# import wkf
-# import history
-import clv_pharmacy
-import clv_professional
-import clv_insured_card
-# import clv_medicament_template
+from openerp import models, fields, api
+
+class clv_medicament_dispensation_mng(models.Model):
+    _inherit = 'clv_medicament_dispensation_mng'
+
+    medicament_ref = fields.Reference([('clv_medicament', 'Medicament'),
+                                       ], 'Medicament Reference')
