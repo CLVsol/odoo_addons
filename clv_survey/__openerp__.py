4a194c884ac3cb91f8f510ad031f2edcb378ff1b
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-09-13 11:58:45 -0300

    Updated "clv_survey".

--- clv_survey/__openerp__.py
+++ clv_survey/__openerp__.py
@@ -43,7 +43,7 @@ user name and password for the invitation of the survey.
         'clv_survey_report.xml',
         #'clv_survey_data.xml',
         'wizard/clv_survey_selection.xml',
-        'wizard/clv_survey_answer.xml',
+        # 'wizard/clv_survey_answer.xml',
         'security/clv_survey_security.xml',
         'security/ir.model.access.csv',
         'clv_survey_view.xml',
