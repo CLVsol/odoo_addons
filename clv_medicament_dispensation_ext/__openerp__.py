ce32f55d6fd541a5989c11af210d7e0cd822e553
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-09-22 10:24:36 -0300

    Updated "clv_medicament_dispensation_ext".

--- clv_medicament_dispensation_ext/__openerp__.py
+++ clv_medicament_dispensation_ext/__openerp__.py
@@ -38,7 +38,7 @@
         'security/clv_medicament_dispensation_ext_security.xml',
         # 'security/ir.model.access.csv',
         'clv_medicament_dispensation_ext_view.xml',
-        # 'category/clv_medicament_dispensation_category_view.xml',
+        'category/clv_medicament_dispensation_ext_category_view.xml',
         # 'clv_tag/clv_tag_view.xml',
         # 'clv_annotation/clv_annotation_view.xml',
         # 'seq/clv_medicament_dispensation_sequence.xml',
