2c7c1ce4dc9085d0b033d41f8fe89396b2e0e1b8
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-11-04 15:00:43 -0200

    Updated "clv_person".

--- clv_person/clv_address/__init__.py
+++ clv_person/clv_address/__init__.py
@@ -17,45 +17,4 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.        #
 ################################################################################
 
-{
-    'name': 'Person',
-    'version': '1.0',
-    'author': 'Carlos Eduardo Vercelino - CLVsol',
-    'category': 'Generic Modules/Others',
-    'license': 'AGPL-3',
-    'website': 'http://clvsol.com',
-    'description': '''
-Person
-======
-    ''',
-    'images': [],
-    'depends': [
-        'clv_base',
-        'clv_tag',
-        'clv_annotation',
-        'clv_address',
-        ],
-    'data': [
-        'security/clv_person_security.xml',
-        'security/ir.model.access.csv',
-        'clv_person_view.xml',
-        'category/clv_person_category_view.xml',
-        'clv_tag/clv_tag_view.xml',
-        'clv_annotation/clv_annotation_view.xml',
-        'seq/clv_person_sequence.xml',
-        'seq/clv_person_category_sequence.xml',
-        'wkf/clv_person_workflow.xml',
-        'wkf/clv_person_wkf_view.xml',
-        'history/clv_person_history_view.xml',
-        'menu/clv_person_menu_view.xml',
-        'clv_address/clv_address_view.xml',
-        ],
-    'demo': [],
-    'test': [],
-    'init_xml': [],
-    'test': [],
-    'update_xml': [],
-    'installable': True,
-    'active': False,
-    'css': [],
-}
+import clv_address
