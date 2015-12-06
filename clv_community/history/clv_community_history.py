36b94db27c60c5e6b84cf40ba0d93704790861b3
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2015-12-06 15:26:21 -0200

    Updated "clv_community".

--- clv_community/history/clv_community_history.py
+++ clv_community/history/clv_community_history.py
@@ -19,7 +19,7 @@
 
 from openerp.osv import fields, osv
 from datetime import *
-#import time
+
 
 class clv_community_history(osv.osv):
     _name = 'clv_community.history'
@@ -28,16 +28,16 @@ class clv_community_history(osv.osv):
 
     _columns = {
         'community_id': fields.many2one('clv_community', 'community', required=True, ondelete='cascade'),
-        'user_id':fields.many2one ('res.users', 'User', required=True),
+        'user_id': fields.many2one('res.users', 'User', required=True),
         'date': fields.datetime("Date", required=True),
-        'state': fields.selection([('new','New'),
-                                   ('active','active'),
-                                   ('inactive','Inactive'),
-                                   ('suspended','Suspended'),
+        'state': fields.selection([('new', 'New'),
+                                   ('active', 'Active'),
+                                   ('inactive', 'Inactive'),
+                                   ('suspended', 'Suspended'),
                                    ], string='Status', readonly=True, required=True, help=""),
         'notes': fields.text(string='Notes'),
         }
-    
+
     _defaults = {
         'user_id': lambda self: self._uid,
         'date': lambda *a: datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
@@ -46,15 +46,17 @@ class clv_community_history(osv.osv):
 
     _order = "date desc"
 
+
 class clv_community(osv.osv):
     _inherit = 'clv_community'
 
     _columns = {
         'history_ids': fields.one2many('clv_community.history', 'community_id', 'Community History', readonly=True),
-        'active_history': fields.boolean('Active History', 
-                                         help="If unchecked, it will allow you to disable the history without removing it."),
+        'active_history': fields.boolean(
+            'Active History',
+            help="If unchecked, it will allow you to disable the history without removing it."),
         }
-    
+
     _defaults = {
         'active_history': True
         }
@@ -64,7 +66,7 @@ class clv_community(osv.osv):
             context = {}
         date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         if active_history:
-            vals = { 
+            vals = {
                 'community_id': community_id,
                 'user_id': uid,
                 'date': date,
@@ -76,41 +78,39 @@ class clv_community(osv.osv):
     def write(self, cr, uid, ids, vals, context=None):
         if context is None:
             context = {}
-        if (not 'state' in vals) and (not 'date' in vals) and (not 'history_ids' in vals):
+        if ('state' not in vals) and ('date' not in vals) and ('history_ids' not in vals):
             notes = vals.keys()
             try:
                 for community in self.browse(cr, uid, ids):
                     if 'active_history' in vals:
                         self.insert_clv_community_history(cr, uid, True, community.id, community.state, notes)
                     else:
-                        self.insert_clv_community_history(cr, uid, community.active_history, community.id, community.state, notes)
+                        self.insert_clv_community_history(cr, uid, community.active_history, community.id,
+                                                          community.state, notes)
             except:
                 pass
         return super(clv_community, self).write(cr, uid, ids, vals, context)
 
     def button_new(self, cr, uid, ids):
-        self.write(cr, uid, ids, {'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 
+        self.write(cr, uid, ids, {'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
                                   'state': 'new'})
         for community in self.browse(cr, uid, ids):
             self.insert_clv_community_history(cr, uid, community.active_history, community.id, 'new', '')
 
     def button_activate(self, cr, uid, ids):
         self.write(cr, uid, ids, {'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                                  'date_activation':  datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
                                   'state': 'active'})
         for community in self.browse(cr, uid, ids):
             self.insert_clv_community_history(cr, uid, community.active_history, community.id, 'active', '')
 
     def button_inactivate(self, cr, uid, ids):
-        self.write(cr, uid, ids, {'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 
-                                  'date_inactivation':  datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        self.write(cr, uid, ids, {'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
                                   'state': 'inactive'})
         for community in self.browse(cr, uid, ids):
             self.insert_clv_community_history(cr, uid, community.active_history, community.id, 'inactive', '')
 
     def button_suspend(self, cr, uid, ids):
-        self.write(cr, uid, ids, {'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 
-                                  'date_suspension':  datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        self.write(cr, uid, ids, {'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
                                   'state': 'suspended'})
         for community in self.browse(cr, uid, ids):
             self.insert_clv_community_history(cr, uid, community.active_history, community.id, 'suspended', '')
